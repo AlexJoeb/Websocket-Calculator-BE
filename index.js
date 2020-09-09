d0a8b60c0b483d9ea2585cc60a5f37ac05ce2bba
Alexander Besse <alexanderjoeb@hotmail.com>
2020-09-09 17:06:55 -0500

    Setup SocketIO and allowed communication of equations

--- index.js
+++ index.js
@@ -11,10 +11,24 @@ app.get('/', (req, res) => {
     })
 })
 
+let lastTen = [];
+
 io.on('connection', socket => {
-    console.log("- A user connected.");
-    io.emit("welcome", { message: "Welcome!" })
-    io.on('disconnect', socket => {
+    // Sends the user the current list on connection.
+    socket.emit("NewList", lastTen);
+
+    // When the client sends the server a 'NewEquation' message, the equation is added and the new list is sent to all clients.
+    socket.on('NewEquation', ({ equation }) => {
+        // Remove the front element is there is 10 or more items, before new item is appended.
+        if(lastTen.length >= 10) lastTen = lastTen.slice(1);
+        lastTen.unshift({
+            ...equation,
+            time: Date.now()
+        }); // Append new item.
+
+        io.sockets.emit("NewList", lastTen);
+    })
+    socket.on('disconnect', socket => {
         console.log("- A user disconnected.");
     })
 })
